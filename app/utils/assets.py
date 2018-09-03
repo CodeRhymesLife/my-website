901617951ed78134d2aaa8e38a982ead84b81a11
Ryan James <rcjames1004@gmail.com>
2018-09-02 20:54:50 -0700

    Add software page

--- app/utils/assets.py
+++ app/utils/assets.py
@@ -12,6 +12,16 @@ bundles = {
         'css/common.css',
         'css/index.css',
         output='gen/index.css'),
+
+    'software_js': Bundle(
+        'js/common.js',
+        output='gen/software.js'),
+
+    'software_css': Bundle(
+        'css/bootstrap-extensions.css',
+        'css/common.css',
+        'css/software.css',
+        output='gen/software.css'),
 }
 
 assets = Environment(app)
