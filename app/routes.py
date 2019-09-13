3cd80ae43d9a94fb3db98159e30d84bc7b2ec2a1
Ryan James <rcjames1004@gmail.com>
2019-09-13 11:57:08 -0700

    Update tiles on index page

--- app/routes.py
+++ app/routes.py
@@ -2,51 +2,49 @@ from flask import render_template, url_for
 
 from app import app
 
-
 @app.route('/')
 @app.route('/index')
 def index():
+    projects = [
+        {
+            'title': 'Telerobotics',
+            'type': 'Album',
+            'img': 'static/images/tiles/telerobotics.png',
+            'url': 'https://photos.app.goo.gl/g1MVGr28j87cH8am7',
+        },
+        {
+            'title': 'VR Guidance',
+            'type': 'Article',
+            'img': 'static/images/tiles/catheye.png',
+            'url': 'https://medium.com/@ryanjameshealth/a-virtual-reality-surgical-guidance-system-9621e4b7b9ea',
+        },
+        {
+            'title': 'VR Planning',
+            'type': 'Video',
+            'img': 'static/images/tiles/bosc.png',
+            'url': 'https://www.youtube.com/watch?v=H1NS6GyttLg',
+        },
+        {
+            'title': 'Model Service',
+            'type': 'GitHub',
+            'img': 'static/images/tiles/3d-model-service.png',
+            'url': '',
+        },
+        {
+            'title': 'Simulation',
+            'type': 'Album',
+            'img': 'static/images/tiles/gallery.png',
+            'url': 'https://photos.app.goo.gl/KKUFqXMm7V1jNt5J8',
+        },
+    ]
+
+    isReady = lambda p: 'url' in p.keys() and p['url']
+
     return render_template(
         'index.html',
         title='Ryan James',
-        tileRows = [
-            [
-                {
-                    'title': 'Telerobotics',
-                    'type': 'Article',
-                    'img': 'static/images/tiles/catheye.png',
-                    'url': '#',
-                },
-                {
-                    'title': 'Guidance',
-                    'type': 'Article',
-                    'img': 'static/images/tiles/catheye.png',
-                    'url': '#',
-                },
-            ],
-            [
-                {
-                    'title': 'Planning',
-                    'type': 'Video',
-                    'img': 'static/images/tiles/bosc.png',
-                    'url': 'https://www.youtube.com/watch?v=H1NS6GyttLg',
-                },
-                {
-                    'title': 'Model Service',
-                    'type': 'GitHub',
-                    'img': 'static/images/tiles/catheye.png',
-                    'url': '#',
-                },
-            ],
-            [
-                {
-                    'title': 'Gallery',
-                    'type': 'Photos',
-                    'img': 'static/images/tiles/gallery.png',
-                    'url': url_for('gallery'),
-                },
-            ],
-        ]
+        readyProjects=filter(isReady, projects),
+        comingSoonProjects=filter(lambda p: not isReady(p), projects),
     )
 
 @app.route('/aboutMe')
