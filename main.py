1cb49c25dab543be577fd25a18c0f5fde0861e33
Ryan James <rcjames1004@gmail.com>
2021-02-10 00:45:18 -0800

    Bind to

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from os import environ as env
 from app import app
 
 if __name__ == '__main__':
-    PORT = int(env.get("HTTP_PORT", 8080))
+    PORT = int(env.get("PORT", 8080))
     app.run(debug=True, host='0.0.0.0', port=PORT)
